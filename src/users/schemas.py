6acf2ef554fc0e9213e8c21be64ca81ff8104821
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-05-18 16:03:22 +0800

    Update schemas.py для регистрации пользователя

--- src/users/schemas.py
+++ src/users/schemas.py
@@ -1,1 +1,14 @@
+from pydantic import BaseModel
 
+class User(BaseModel):
+    login: str
+
+class CreateUser(User):
+    password: str
+
+class Login(User):
+    password: str
+
+class Response(BaseModel):
+    success: bool
+    accessToken: str
