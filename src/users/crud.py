c78670a08589a96adca0fee7e98947f887fdc481
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-05-18 16:08:23 +0800

    Update crud.py создание пользователя и поиск пользователя

--- src/users/crud.py
+++ src/users/crud.py
@@ -1,1 +1,18 @@
+from sqlalchemy.orm import Session
+from users import models, schemas
 
+import uuid
+from typing import Optional
+
+def create_user(db: Session, user: schemas.CreateUser) -> Optional[models.usersT]:
+    password = user.password
+    accessToken = str(uuid.uuid4())[:8]
+    db.user = models.usersT(login=user.login, password=password, accessToken=accessToken)
+    db.add(db.user)
+    db.commit()
+    db.refresh(db.user)
+    return db.user
+
+def find_user(db: Session, login: str) -> Optional[models.usersT]:
+    user = db.query(models.usersT).filter_by(login=login).first()
+    return user
