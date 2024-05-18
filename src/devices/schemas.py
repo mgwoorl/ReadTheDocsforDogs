6c704190b66a6cb2196dec3193ec9c22b686dae3
mgwoorl <149603747+mgwoorl@users.noreply.github.com>
2024-05-18 21:53:18 +0800

    Update schemas.py

--- src/devices/schemas.py
+++ src/devices/schemas.py
@@ -1,1 +1,11 @@
+from pydantic import BaseModel
 
+class Dog(BaseModel):
+    name: str
+
+class CreateDog(Dog):
+    collar_id: str
+
+class ResponseDog(BaseModel):
+    success: bool
+    collar_token: str
