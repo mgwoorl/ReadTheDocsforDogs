f4614ef01586bdaee4c8b269e1060840f6a9d0c8
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-05-18 16:06:26 +0800

    Update router.py для регистрации пользователя

--- src/users/router.py
+++ src/users/router.py
@@ -1,1 +1,17 @@
+import users.crud as crud
+import users.schemas as schemas
+import users.exceptions as exceptions
+from dependecies import session
+from database import DBSession
 
+from fastapi import APIRouter, Depends
+
+router = APIRouter()
+
+@router.post("/user/register", response_model=schemas.Response)
+async def UserRegistration(user: schemas.CreateUser, db: DBSession = Depends(session)):
+    if crud.find_user(db, user.login):
+        raise exceptions.BusyLogin()
+    new_user = crud.create_user(db, user)
+    result = schemas.Response(success=True, accessToken=new_user.accessToken)
+    return result
