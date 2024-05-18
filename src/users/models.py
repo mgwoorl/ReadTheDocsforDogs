2c5bf4a28a1789411ba7709cdcf205e3b4cff593
lemongoose228 <150834453+lemongoose228@users.noreply.github.com>
2024-05-18 16:06:55 +0800

    Update models.py таблица пользователи

--- src/users/models.py
+++ src/users/models.py
@@ -1,1 +1,9 @@
+from sqlalchemy import Integer, String, Column
+from database import BaseDBModel
 
+class usersT(BaseDBModel):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    login = Column(String, unique=True, index=True)
+    password = Column(String, unique=False, index=False)
+    accessToken = Column(String, unique=False, index=False)
